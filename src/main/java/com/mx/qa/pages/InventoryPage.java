4b99692b8cba7ae9838a5fc13f97ec2062f72d12
Steven_Huasupoma <shuasupomaolaya@gmail.com>
2023-03-07 01:59:17 -0600

    Terminando Inventory Page

--- src/main/java/com/mx/qa/pages/InventoryPage.java
+++ src/main/java/com/mx/qa/pages/InventoryPage.java
@@ -1,4 +1,35 @@
 package com.mx.qa.pages;
 
-public class InventoryPage {
+import com.mx.qa.base.TestBase;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class InventoryPage extends TestBase {
+    @FindBy(xpath = "//a[contains(@class,inventory_item)]//div[contains(text(),'Backpack')]")
+    public WebElement nameProduct;
+
+    @FindBy(xpath = "(//div[contains(@class,'pricebar')])[position()=1]")
+    public WebElement priceProduct;
+
+    @FindBy(id = "add-to-cart-sauce-labs-backpack")
+    public WebElement addCart;
+
+    @FindBy(xpath = "//a[@class='shopping_cart_link']")
+    public WebElement btnCar;
+
+    public String name_Product = nameProduct.getText();
+    public String price_Product = priceProduct.getText();
+
+    public void agregarCarrito() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", addCart);
+    }
+
+    public void irCarrito() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", btnCar);
+    }
+
+
 }
