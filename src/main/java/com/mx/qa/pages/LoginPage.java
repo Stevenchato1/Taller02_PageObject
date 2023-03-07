2db5d50dbce8a8bb012a8ab15590d7d43db97fea
Steven_Huasupoma <shuasupomaolaya@gmail.com>
2023-03-07 00:44:49 -0600

    Terminando LoginPage

--- src/main/java/com/mx/qa/pages/LoginPage.java
+++ src/main/java/com/mx/qa/pages/LoginPage.java
@@ -1,4 +1,32 @@
 package com.mx.qa.pages;
 
-public class LoginPage {
+import com.mx.qa.base.TestBase;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends TestBase {
+
+    @FindBy(id = "user-name")
+    public WebElement users;
+
+    @FindBy(id = "password" )
+    public WebElement passwords;
+
+    @FindBy(id = "login-button")
+    public WebElement login;
+
+    public LoginPage(){
+        PageFactory.initElements(driver, this);
+    }
+
+    public LoginPage enterLogin(String user, String password){
+        users.sendKeys(user);
+        passwords.sendKeys(password);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", login);
+        return new LoginPage();
+    }
+
 }
