6205e7d7f1c6c8329db710a300864391277b5c74
Steven_Huasupoma <shuasupomaolaya@gmail.com>
2023-03-07 00:25:08 -0600

    Clase TesBase terminado

--- src/main/java/com/mx/qa/base/TestBase.java
+++ src/main/java/com/mx/qa/base/TestBase.java
@@ -1,4 +1,41 @@
 package com.mx.qa.base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class TestBase {
+    public static WebDriver driver;
+    public static Properties properties;
+
+    public TestBase() {
+        try {
+            properties = new Properties();
+            FileInputStream ip = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/com/pe/qa/config/config.properties");
+            properties.load(ip);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void initialization() {
+        String browserName = properties.getProperty("browser");
+        if (browserName.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browserName.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        driver.manage().window().maximize();
+        driver.get(properties.getProperty("url"));
+    }
 }
