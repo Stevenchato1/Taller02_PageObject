e4e6af5cdad5beb1cf940986c8acbe712998b604
Steven_Huasupoma <shuasupomaolaya@gmail.com>
2023-03-07 23:56:33 -0600

    Terminando taller02

--- src/test/java/com/mx/tescases/ValidarOrderProduct.java
+++ src/test/java/com/mx/tescases/ValidarOrderProduct.java
@@ -13,6 +13,7 @@ public class ValidarOrderProduct extends TestBase {
     CarPage carPage;
     CheckoutOnePage checkoutOnePage;
     CheckoutTwoPage checkoutTwoPage;
+    CheckoutFinishPage checkoutFinishPage;
 
     public ValidarOrderProduct(){super();}
 
@@ -23,22 +24,41 @@ public class ValidarOrderProduct extends TestBase {
         inventoryPage = new InventoryPage();
         carPage = new CarPage();
         checkoutOnePage=new CheckoutOnePage();
+        checkoutTwoPage=new CheckoutTwoPage();
+        checkoutFinishPage=new CheckoutFinishPage();
     }
 
     @Test
-    public void validateProduct(){
+    public void validateProductinOrder(){
         loginPage.enterLogin("standard_user","secret_sauce");
         String name=inventoryPage.nameProduct.getText();
         String price=inventoryPage.priceProduct.getText();
         inventoryPage.agregarCarrito();
         inventoryPage.irCarrito();
-        boolean flagTrue = carPage.validateProducto(name);
-        Assert.assertTrue(flagTrue);
+        boolean flagTrue1 = carPage.validateProducto(name);
+        Assert.assertTrue(flagTrue1);
 
-        boolean flagPrice = carPage.validatePrecio(price);
-        Assert.assertTrue(flagPrice);
+        boolean flagTrue2 = carPage.validatePrecio(price);
+        Assert.assertTrue(flagTrue2);
         carPage.irCheckout();
+
+        checkoutOnePage.enterUser(properties.getProperty("firstName"), properties.getProperty("lastName"),
+                properties.getProperty("zipCode"));
+        checkoutOnePage.irCheckout();
+       boolean flagTrue3 =checkoutTwoPage.validateProducto(name);
+       Assert.assertTrue(flagTrue3);
+
+       boolean flagTrue4 =  checkoutTwoPage.validatePrecio(price);
+       Assert.assertTrue(flagTrue4);
+        checkoutTwoPage.enterFinish();
+
+        boolean flagTrue5=checkoutFinishPage.validateFinish();
+        Assert.assertTrue(flagTrue5);
+
+
     }
+
+
 
 
 
